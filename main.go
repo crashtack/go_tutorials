972c2f3afaa955ce26472cba6f494e91780aba96
crashtack <crashtack@gmail.com>
2017-01-08 10:15:29 -0800

    Print some random numbers

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("My favorite number is", rand.Intn(999))
+
+	for i:=0; i < 10; i++ {
+		bytes := rand.Intn(10)
+		fmt.Print(bytes)
+	}
 }
